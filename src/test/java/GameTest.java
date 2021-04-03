2e168241631d0ce331089e68ab58ad8a7a8a2502
Allen Hu <allenhu1029@gmail.com>
2021-04-03 21:02:18 +0800

    Add Main-Class

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,8 +1,5 @@
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
